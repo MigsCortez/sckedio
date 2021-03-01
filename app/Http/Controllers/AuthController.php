9e486d264e4c06eb99de4f6431b94b185885ceb7
MigsCortez <migueldlcortez@gmail.com>
2021-03-01 13:23:31 -0800

    U: updated changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,65 +36,12 @@ class AuthController extends Controller
     return $status;
     }
 
-<<<<<<< HEAD
-      public function login(LogInRequest $request) 
-      {
-        // $status = $this->userAuth->handleLogIn($request);
-        // return $status;
-
-        $credentials = request(['username', 'password']);
-        
-        if(!Auth::attempt($credentials))
-        {
-            return response()->json([
-                'message' => 'Unauthorized',
-            ], 401);
-        }
-
-        $username = $request->username;
-        $password = $request->password;
-
-        $tokenRequest = Request::create(
-            env('APP_URL').'/oauth/token',
-            'POST',
-            [
-                'grant_type' => 'password',
-                'client_id' => config('services.passport.password_client_id'),
-                'client_secret' => config('services.passport.password_client_secret'),
-                'username' => $username,
-                'password' => $password,
-                'scope' => '*'
-            ]
-        );
-        $response = app()->handle($tokenRequest);
-        return $response;
-      }
-
-      /**
-       * Logout user (Revoke the token)
-       * @return [string] message
-       */
-
-       public function logout(Request $request) 
-       {
-            $tokenId = $request->user()->token()->id;
-            $status = $this->userAuth->handleLogOut($tokenId);
-            return $status;
-       }
-
-       /**
-        * Get the authenticated user
-        *
-        * @return [json] user object 
-        */
-=======
     public function logout(Request $request) 
     {
         $tokenId = $request->user()->token()->id;
         $status = $this->userAuth->handleLogOut($tokenId);
         return $status;
     }
->>>>>>> a2d8a44789525341a6dc6ff52def7723a2fd79a1
 
     public function user(Request $request) 
     {
